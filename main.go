e0d9e21d0c845bbe99533541b89d81e4ec38fe85
vl <vl@mobitutor.ru>
2020-06-16 10:38:28 +0300

    Fixing total calculation (dividing by 100)

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 func main() {
 	var balance int64 = 15_000_000_00 // 15 миллионов в копейках
 	var transfer int64 = 10_000_000_00 // 10 миллионов в копейках
-	total := (balance + transfer) / 100 // общая сумма ыв рублях
+	total := balance + transfer  // общая сумма в копейках
 	println(total)
 }
